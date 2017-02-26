8f76fe833e931a1bbc6f8709d755c0cbd831e277
waiplushk <cyw1222@gmail.com>
2017-02-26 20:24:27 +0800

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,53 +1,45 @@
-from flask import Flask, request
-import json
-import requests
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "<p>Hello World!</p>"
+line_bot_api = LineBotApi('ceTNL2J4uc+jYPwFzET135bsgOW19/rYwQHyN4Z6w2WZSY0qs3Vu/DvOZzfeg7Y6bFmR1WsjC5v/dBMGGvkjhhIrvdkym45oToPNjnfHBX9iHAQUzN7Rdi8yx1l1tyxXOnxfbVyFrGuIftYg2GwQrgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('db2291b0611e74dfee6b81d66407e509')
 
-@app.route('/callback', methods=['POST'])
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    user = decoded['result'][0]['content']['from']
-    text = decoded['result'][0]['content']['text']
-    #print(json_line)
-    print("使用者：",user)
-    print("內容：",text)
-    sendText(user,text)
-    return ''
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-def sendText(user, text):
-    LINE_API = 'https://trialbot-api.line.me/v1/events'
-    CHANNEL_ID = '1503034805'
-    CHANNEL_SERECT = 'db2291b0611e74dfee6b81d66407e509'
-    MID = 'ceTNL2J4uc+jYPwFzET135bsgOW19/rYwQHyN4Z6w2WZSY0qs3Vu/DvOZzfeg7Y6bFmR1WsjC5v/dBMGGvkjhhIrvdkym45oToPNjnfHBX9iHAQUzN7Rdi8yx1l1tyxXOnxfbVyFrGuIftYg2GwQrgdB04t89/1O/w1cDnyilFU='
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'X-Line-ChannelID': CHANNEL_ID,
-        'X-Line-ChannelSecret': CHANNEL_SERECT,
-        'X-Line-Trusted-User-With-ACL': MID
-    }
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-    data = json.dumps({
-        "to": [user],
-        "toChannel":1383378250,
-        "eventType":"138311608800106203",
-        "content":{
-            "contentType":1,
-            "toType":1,
-            "text":text
-        }
-    })
+    return 'OK'
 
-    #print("送出資料：",data)
-    r = requests.post(LINE_API, headers=headers, data=data)
-    #print(r.text)
 
-if __name__ == '__main__':
-     app.run(debug=True)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
